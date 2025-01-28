e5fc70b891392d066b6c6e955f7fbbb5f22c1958
Akintola-Stephen <sakintola351@gmail.com>
2025-01-28 23:42:31 +0100

    feat: add new modules to the project structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './modules/product/product.module';
+import { ProductCategoryModule } from './modules/product-category/product-category.module';
+import { PlanModule } from './modules/plan/plan.module';
+import { PolicyModule } from './modules/policy/policy.module';
+import { UserModule } from './modules/user/user.module';
+import { WalletModule } from './modules/wallet/wallet.module';
 
 @Module({
-  imports: [],
+  imports: [ProductModule, ProductCategoryModule, PlanModule, PolicyModule, UserModule, WalletModule],
   controllers: [AppController],
   providers: [AppService],
 })
