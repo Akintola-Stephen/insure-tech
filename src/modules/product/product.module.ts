d3a7042e30c347569405df6f7ac4f1348d88102c
Akintola-Stephen <sakintola351@gmail.com>
2025-01-29 09:10:44 +0100

    feat: add ProductModule with service, controller, and Sequelize integration

--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Product } from './product.model';
+import { ProductController } from './product.controller';
+import { ProductService } from './product.service';
 
-@Module({})
-export class ProductModule {}
+@Module({
+    imports: [
+        SequelizeModule.forFeature([Product])
+    ],
+    controllers: [ProductController],
+    providers: [ProductService],
+    exports: [ProductService]
+})
+export class ProductModule { }
