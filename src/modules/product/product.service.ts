e5c3b92e88dd7690eedc3cf9d145f9e5f05daf7e
Akintola-Stephen <sakintola351@gmail.com>
2025-01-29 08:45:05 +0100

    feat: add method to fetch product with its category by name

---
+++ src/modules/product/product.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { InjectModel } from "@nestjs/sequelize";
+import { Product } from "./product.model";
+import { ProductCategory } from "../product-category/product-category.model";
+
+@Injectable()
+export class ProductService {
+    constructor(@InjectModel(Product) private productModel: typeof Product) { }
+
+
+    //TODO: Function that creates new product
+
+    //TODO: Function that fetches every products
+
+    async getProductWithCategoryByName(productName: string): Promise<Product> {
+        const product = await this.productModel.findOne({
+            where: {
+                productName
+            },
+            include: [
+                { model: ProductCategory, as: "category" }
+            ]
+        });
+        if (!product) throw new NotFoundException(`Product with name ${productName} not found`)
+        return product
+    }
+}
